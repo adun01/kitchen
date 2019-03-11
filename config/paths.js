33a547996ebf8c5e05045f6b938b35632c291509
alexey <ubaev.alexey@gmail.com>
2019-03-12 01:27:37 +0300

    rebuild config for dev, remove dev-server

--- config/paths.js
+++ config/paths.js
@@ -69,7 +69,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  appBuild: resolveApp('../kitchen-serv/public/'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
